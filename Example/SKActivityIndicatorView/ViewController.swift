ff3cd99e7400bb059e5425320dfdc55ff68f24f1
Sachin Kadam <iosdev.sachin@gmail.com>
2017-12-22 00:20:13 +0530

    Create Sample Project

--- Example/SKActivityIndicatorView/ViewController.swift
+++ Example/SKActivityIndicatorView/ViewController.swift
@@ -7,18 +7,70 @@
 //
 
 import UIKit
+import SKActivityIndicatorView
 
 class ViewController: UIViewController {
-
+    
+    // Outlet
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    // Custom Properties
+    var statusMessage = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        SKActivityIndicator.spinnerColor(UIColor.darkGray)
+        SKActivityIndicator.statusTextColor(UIColor.black)
+        let myFont = UIFont(name: "AvenirNext-DemiBold", size: 18)
+        SKActivityIndicator.statusLabelFont(myFont!)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - SKActivityIndicator Styles
+    @IBAction func showActivityIndicatorStyle1ButtonTapped(_ sender: Any) {
+        SKActivityIndicator.spinnerStyle(.defaultSpinner)
+        if let message = messageTextField.text {
+            statusMessage = message
+        }
+        SKActivityIndicator.show(statusMessage, userInteractionStatus: true)
     }
-
+    
+    @IBAction func showActivityIndicatorStyle2ButtonTapped(_ sender: Any) {
+        SKActivityIndicator.spinnerStyle(.spinningCircle)
+        if let message = messageTextField.text {
+            statusMessage = message
+        }
+        SKActivityIndicator.show(statusMessage, userInteractionStatus: true)
+    }
+    
+    @IBAction func showActivityIndicatorStyle3ButtonTapped(_ sender: Any) {
+        SKActivityIndicator.spinnerStyle(.spinningFadeCircle)
+        if let message = messageTextField.text {
+            statusMessage = message
+        }
+        SKActivityIndicator.show(statusMessage, userInteractionStatus: true)
+    }
+    
+    @IBAction func showActivityIndicatorStyle4ButtonTapped(_ sender: Any) {
+        SKActivityIndicator.spinnerStyle(.spinningHalfCircles)
+        if let message = messageTextField.text {
+            statusMessage = message
+        }
+        SKActivityIndicator.show(statusMessage, userInteractionStatus: true)
+    }
+    
+    @IBAction func hideActivityIndicatorButtonTapped(_ sender: Any) {
+        SKActivityIndicator.dismiss()
+    }
 }
 
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
