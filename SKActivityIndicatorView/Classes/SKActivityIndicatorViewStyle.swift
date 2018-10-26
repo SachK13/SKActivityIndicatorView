86c3551ba74e6bfc980834e22f0c232e0ecb35c0
Armin Telker <a.telker@rebuy.de>
2018-10-26 14:02:46 +0200

    update SKActivityIndicatorViewStyle to swift 4.2

--- SKActivityIndicatorView/Classes/SKActivityIndicatorViewStyle.swift
+++ SKActivityIndicatorView/Classes/SKActivityIndicatorViewStyle.swift
@@ -17,37 +17,35 @@ public enum ActivityIndicatorStyle {
 }
 
 class SKActivityIndicatorStyle: NSObject {
-    
     static let shared = SKActivityIndicatorStyle()
     private override init() {}
-    
+
     /**
      Create Activity-Indicator based on provided style
-     
+
      - parameter layer : Spinner containerview layer
      - parameter size  : Size of spinner
      - parameter color : Color of spinner, default is "lightGray"
      - parameter style : Spinner style specified by user, default style is "defaultSpinner"
      */
     class func createSpinner(in layer: CALayer, size: CGSize, color: UIColor, style: ActivityIndicatorStyle) {
-        
-        switch  style {
+        switch style {
         case .defaultSpinner:
-            self.shared.createCircularLineFadingActivityIndicator(in: layer, size: size, color: color)
-            
+            shared.createCircularLineFadingActivityIndicator(in: layer, size: size, color: color)
+
         case .spinningFadeCircle:
-            self.shared.createCircularSpinningBallWithFadingActivityIndicator(in: layer, size: size, color: color)
-            
+            shared.createCircularSpinningBallWithFadingActivityIndicator(in: layer, size: size, color: color)
+
         case .spinningCircle:
-            self.shared.createCircularSpinninBallActivityIndicator(in: layer, size: size, color: color)
-            
+            shared.createCircularSpinninBallActivityIndicator(in: layer, size: size, color: color)
+
         case .spinningHalfCircles:
-            self.shared.createTwoHalfCircleSpinningActivityIndicator(in: layer, size: size, color: color)
+            shared.createTwoHalfCircleSpinningActivityIndicator(in: layer, size: size, color: color)
         }
     }
-    
+
     // MARK: - Create Spinner
-    
+
     // ---------------------------------------------------Line Fade Spinner ---------------------------------------------- //
     // ------------------------------------------------------------------------------------------------------------------- //
     func createCircularLineFadingActivityIndicator(in layer: CALayer, size: CGSize, color: UIColor) {
@@ -58,8 +56,8 @@ class SKActivityIndicatorStyle: NSObject {
         let duration: CFTimeInterval = 0.5
         let beginTime = CACurrentMediaTime()
         let beginTimes: [CFTimeInterval] = [0.12, 0.24, 0.36, 0.48, 0.6, 0.72, 0.84, 0.96, 0.108, 0.120]
-        let timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        
+        let timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+
         // Animation
         let animation = CAKeyframeAnimation(keyPath: "opacity")
         animation.keyTimes = [0, 0.5, 1]
@@ -68,7 +66,7 @@ class SKActivityIndicatorStyle: NSObject {
         animation.duration = duration
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-        
+
         // Draw lines
         for i in 0 ..< 10 {
             let line = lineAt(angle: CGFloat(Double.pi / 5 * Double(i)),
@@ -76,13 +74,13 @@ class SKActivityIndicatorStyle: NSObject {
                               origin: CGPoint(x: x, y: y),
                               containerSize: size,
                               color: color)
-            
+
             animation.beginTime = beginTime + beginTimes[i]
             line.add(animation, forKey: "animation")
             layer.addSublayer(line)
         }
     }
-    
+
     func lineAt(angle: CGFloat, size: CGSize, origin: CGPoint, containerSize: CGSize, color: UIColor) -> CALayer {
         let radius = containerSize.width / 2 - max(size.width, size.height) / 2
         let lineContainerSize = CGSize(width: max(size.width, size.height), height: max(size.width, size.height))
@@ -91,43 +89,43 @@ class SKActivityIndicatorStyle: NSObject {
             x: origin.x + radius * (cos(angle) + 1),
             y: origin.y + radius * (sin(angle) + 1),
             width: lineContainerSize.width,
-            height: lineContainerSize.height)
+            height: lineContainerSize.height
+        )
         let line = circularLineFadingActivityIndicatorlayerWith(size: size, color: color)
         let lineFrame = CGRect(
             x: (lineContainerSize.width - size.width) / 2,
             y: (lineContainerSize.height - size.height) / 2,
             width: size.width,
-            height: size.height)
-        
+            height: size.height
+        )
+
         lineContainer.frame = lineContainerFrame
         line.frame = lineFrame
         lineContainer.addSublayer(line)
         lineContainer.sublayerTransform = CATransform3DMakeRotation(CGFloat(Double.pi / 2) + angle, 0, 0, 1)
-        
+
         return lineContainer
     }
-    
+
     func circularLineFadingActivityIndicatorlayerWith(size: CGSize, color: UIColor) -> CALayer {
         let layer: CAShapeLayer = CAShapeLayer()
         var path: UIBezierPath = UIBezierPath()
-        
-        path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: size.width, height: size.height),cornerRadius: size.width / 2)
+
+        path = UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: size.width, height: size.height), cornerRadius: size.width / 2)
         layer.fillColor = color.cgColor
-        
+
         layer.backgroundColor = nil
         layer.path = path.cgPath
         layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
+
         return layer
     }
-    
+
     // ------------------------------------------------------------------------------------------------------------------- //
-    
-    
-    
+
     // ---------------------------------------------------Ball Spin Fade Spinner ----------------------------------------- //
     // ------------------------------------------------------------------------------------------------------------------- //
-    
+
     func createCircularSpinningBallWithFadingActivityIndicator(in layer: CALayer, size: CGSize, color: UIColor) {
         let circleSpacing: CGFloat = -2
         let circleSize = (size.width - 4 * circleSpacing) / 5
@@ -136,30 +134,30 @@ class SKActivityIndicatorStyle: NSObject {
         let duration: CFTimeInterval = 1
         let beginTime = CACurrentMediaTime()
         let beginTimes: [CFTimeInterval] = [0, 0.12, 0.24, 0.36, 0.48, 0.6, 0.72, 0.84]
-        
+
         // Scale animation
         let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        
+
         scaleAnimation.keyTimes = [0, 0.5, 1]
         scaleAnimation.values = [1, 0.4, 1]
         scaleAnimation.duration = duration
-        
+
         // Opacity animation
         let opacityAnimaton = CAKeyframeAnimation(keyPath: "opacity")
-        
+
         opacityAnimaton.keyTimes = [0, 0.5, 1]
         opacityAnimaton.values = [1, 0.3, 1]
         opacityAnimaton.duration = duration
-        
+
         // Animation
         let animation = CAAnimationGroup()
-        
+
         animation.animations = [scaleAnimation, opacityAnimaton]
-        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
         animation.duration = duration
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-        
+
         // Draw circles
         for i in 0 ..< 8 {
             let circle = circleAt(angle: CGFloat(Double.pi / 4) * CGFloat(i),
@@ -167,13 +165,13 @@ class SKActivityIndicatorStyle: NSObject {
                                   origin: CGPoint(x: x, y: y),
                                   containerSize: size,
                                   color: color)
-            
+
             animation.beginTime = beginTime + beginTimes[i]
             circle.add(animation, forKey: "animation")
             layer.addSublayer(circle)
         }
     }
-    
+
     func circleAt(angle: CGFloat, size: CGFloat, origin: CGPoint, containerSize: CGSize, color: UIColor) -> CALayer {
         let radius = containerSize.width / 2 - size / 2
         let circle = spinningBallWithFadingActivityIndicatorlayerWith(size: CGSize(width: size, height: size), color: color)
@@ -181,39 +179,38 @@ class SKActivityIndicatorStyle: NSObject {
             x: origin.x + radius * (cos(angle) + 1),
             y: origin.y + radius * (sin(angle) + 1),
             width: size,
-            height: size)
-        
+            height: size
+        )
+
         circle.frame = frame
         return circle
     }
-    
+
     func spinningBallWithFadingActivityIndicatorlayerWith(size: CGSize, color: UIColor) -> CALayer {
         let layer: CAShapeLayer = CAShapeLayer()
         let path: UIBezierPath = UIBezierPath()
-        
+
         path.addArc(withCenter: CGPoint(x: size.width / 2, y: size.height / 2),
                     radius: size.width / 2,
                     startAngle: 0,
                     endAngle: CGFloat(2 * Double.pi),
                     clockwise: false)
         layer.fillColor = color.cgColor
-        
+
         layer.backgroundColor = nil
         layer.path = path.cgPath
         layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
+
         return layer
     }
-    
+
     // ------------------------------------------------------------------------------------------------------------------- //
-    
-    
-    
+
     // ---------------------------------------------------Ball Spin Spinner ---------------------------------------------- //
     // ------------------------------------------------------------------------------------------------------------------- //
     func createCircularSpinninBallActivityIndicator(in layer: CALayer, size: CGSize, color: UIColor) {
         let circleSize = size.width / 4
-        
+
         // Draw circles
         for i in 0 ..< 5 {
             let factor = Float(i) * 1 / 5
@@ -224,26 +221,26 @@ class SKActivityIndicatorStyle: NSObject {
             layer.addSublayer(circle)
         }
     }
-    
+
     func rotateAnimation(_ rate: Float, x: CGFloat, y: CGFloat, size: CGSize) -> CAAnimationGroup {
         let duration: CFTimeInterval = 1.2
         let fromScale = 1 - rate
         let toScale = 0.2 + rate
         let timeFunc = CAMediaTimingFunction(controlPoints: 0.5, 0.15 + rate, 0.25, 1)
-        
+
         // Scale animation
         let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
         scaleAnimation.duration = duration
         scaleAnimation.repeatCount = HUGE
         scaleAnimation.fromValue = fromScale
         scaleAnimation.toValue = toScale
-        
+
         // Position animation
         let positionAnimation = CAKeyframeAnimation(keyPath: "position")
         positionAnimation.duration = duration
         positionAnimation.repeatCount = HUGE
         positionAnimation.path = UIBezierPath(arcCenter: CGPoint(x: x, y: y), radius: size.width / 2, startAngle: CGFloat(3 * Double.pi * 0.5), endAngle: CGFloat(3 * Double.pi * 0.5 + 2 * Double.pi), clockwise: true).cgPath
-        
+
         // Aniamtion
         let animation = CAAnimationGroup()
         animation.animations = [scaleAnimation, positionAnimation]
@@ -251,40 +248,38 @@ class SKActivityIndicatorStyle: NSObject {
         animation.duration = duration
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-        
+
         return animation
     }
-    
+
     func circularSpinningBallActivityIndicatorlayerWith(size: CGSize, color: UIColor) -> CALayer {
         let layer: CAShapeLayer = CAShapeLayer()
         let path: UIBezierPath = UIBezierPath()
-        
+
         path.addArc(withCenter: CGPoint(x: size.width / 2, y: size.height / 2),
                     radius: size.width / 2,
                     startAngle: 0,
                     endAngle: CGFloat(2 * Double.pi),
                     clockwise: false)
         layer.fillColor = color.cgColor
-        
+
         layer.backgroundColor = nil
         layer.path = path.cgPath
         layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
+
         return layer
     }
-    
+
     // ------------------------------------------------------------------------------------------------------------------- //
-    
-    
-    
+
     // ---------------------------------------------------Half Circle Spin Spinner ----------------------------------------- //
     // --------------------------------------------------------------------------------------------------------------------- //
     func createTwoHalfCircleSpinningActivityIndicator(in layer: CALayer, size: CGSize, color: UIColor) {
         let bigCircleSize: CGFloat = size.width
         let smallCircleSize: CGFloat = size.width / 2
         let longDuration: CFTimeInterval = 0.8
-        let timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        
+        let timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+
         outerCircleOf(duration: longDuration,
                       timingFunction: timingFunction,
                       layer: layer,
@@ -296,7 +291,7 @@ class SKActivityIndicatorStyle: NSObject {
                       size: smallCircleSize,
                       color: color, reverse: true)
     }
-    
+
     func createAnimationIn(duration: CFTimeInterval, timingFunction: CAMediaTimingFunction, reverse: Bool) -> CAAnimation {
         // Scale animation
         let scaleAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
@@ -304,7 +299,7 @@ class SKActivityIndicatorStyle: NSObject {
         scaleAnimation.timingFunctions = [timingFunction, timingFunction]
         scaleAnimation.values = [1, 1, 1]
         scaleAnimation.duration = duration
-        
+
         // Rotate animation
         let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
         if !reverse {
@@ -316,17 +311,17 @@ class SKActivityIndicatorStyle: NSObject {
             rotateAnimation.toValue = 0.0
             rotateAnimation.duration = duration
         }
-        
+
         // Animation
         let animation = CAAnimationGroup()
         animation.animations = [scaleAnimation, rotateAnimation]
         animation.duration = duration
         animation.repeatCount = HUGE
         animation.isRemovedOnCompletion = false
-        
+
         return animation
     }
-    
+
     func innerCircleOf(duration: CFTimeInterval, timingFunction: CAMediaTimingFunction, layer: CALayer, size: CGFloat, color: UIColor, reverse: Bool) {
         let circle = innerHalfCirclelayerWith(size: CGSize(width: size, height: size), color: color)
         let frame = CGRect(x: (layer.bounds.size.width - size) / 2,
@@ -334,17 +329,17 @@ class SKActivityIndicatorStyle: NSObject {
                            width: size,
                            height: size)
         let animation = createAnimationIn(duration: duration, timingFunction: timingFunction, reverse: reverse)
-        
+
         circle.frame = frame
         circle.add(animation, forKey: "animation")
         layer.addSublayer(circle)
     }
-    
+
     func innerHalfCirclelayerWith(size: CGSize, color: UIColor) -> CALayer {
         let layer: CAShapeLayer = CAShapeLayer()
         let path: UIBezierPath = UIBezierPath()
         let lineWidth: CGFloat = 2
-        
+
         path.addArc(withCenter: CGPoint(x: size.width / 2, y: size.height / 2),
                     radius: size.width / 2,
                     startAngle: CGFloat(-3 * Double.pi / 4),
@@ -362,14 +357,14 @@ class SKActivityIndicatorStyle: NSObject {
         layer.fillColor = nil
         layer.strokeColor = color.cgColor
         layer.lineWidth = lineWidth
-        
+
         layer.backgroundColor = nil
         layer.path = path.cgPath
         layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
+
         return layer
     }
-    
+
     func outerCircleOf(duration: CFTimeInterval, timingFunction: CAMediaTimingFunction, layer: CALayer, size: CGFloat, color: UIColor, reverse: Bool) {
         let circle = outerHalfCirclelayerWith(size: CGSize(width: size, height: size), color: color)
         let frame = CGRect(x: (layer.bounds.size.width - size) / 2,
@@ -377,17 +372,17 @@ class SKActivityIndicatorStyle: NSObject {
                            width: size,
                            height: size)
         let animation = createAnimationIn(duration: duration, timingFunction: timingFunction, reverse: reverse)
-        
+
         circle.frame = frame
         circle.add(animation, forKey: "animation")
         layer.addSublayer(circle)
     }
-    
+
     func outerHalfCirclelayerWith(size: CGSize, color: UIColor) -> CALayer {
         let layer: CAShapeLayer = CAShapeLayer()
         let path: UIBezierPath = UIBezierPath()
         let lineWidth: CGFloat = 2
-        
+
         path.addArc(withCenter: CGPoint(x: size.width / 2, y: size.height / 2),
                     radius: size.width / 2,
                     startAngle: CGFloat(3 * Double.pi / 4),
@@ -405,15 +400,13 @@ class SKActivityIndicatorStyle: NSObject {
         layer.fillColor = nil
         layer.strokeColor = color.cgColor
         layer.lineWidth = lineWidth
-        
+
         layer.backgroundColor = nil
         layer.path = path.cgPath
         layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
+
         return layer
     }
-    
+
     // --------------------------------------------------------------------------------------------------------------------- //
-    
-    
 }
