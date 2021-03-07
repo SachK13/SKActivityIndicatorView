a11e573dac9fb6ff38f1a7fb53a4ff764396c01c
somendra.yadav <45828831+SomuYadav@users.noreply.github.com>
2021-03-08 01:06:19 +0530

    Added SWift Package Manager Support

---
+++ Tests/SKActivityIndicatorViewTests/SKActivityIndicatorViewTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import SKActivityIndicatorView
+
+final class SKActivityIndicatorViewTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SKActivityIndicatorView().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
